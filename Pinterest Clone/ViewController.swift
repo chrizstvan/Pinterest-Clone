169832f0356350863dd4a45e55cd3b32eca18ef9
Christian Stevanus <chris.stev30@gmail.com>
2019-05-15 11:07:01 +0700

    Initial Commit

---
+++ Pinterest Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinterest Clone
+//
+//  Created by Christian Stevanus on 15/05/19.
+//  Copyright © 2019 Christian Stevanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
